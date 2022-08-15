7082c9ae69051460c4c35c206feb3de72b985d8a
Selcuk Goktas <developer@pbo.com>
2022-08-15 17:42:42 +0300

    x

--- js/ui.js
+++ js/ui.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const menus = document.querySelectorAll('.side-menu');
 	M.Sidenav.init(menus, { edge: 'right' });
 
-	// add recipe form
+	// add ship form
 	const forms = document.querySelectorAll('.side-form');
 	M.Sidenav.init(forms, { edge: 'left' });
 });
